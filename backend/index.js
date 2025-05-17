8775da16c04195f3f4f6d74513c54c70707d07df
Prashant Timilsina <157892231+PrashantTimilsina@users.noreply.github.com>
2025-05-17 19:42:31 +0545

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -40,7 +40,7 @@ app.use(xss());
 app.use(hpp());
 
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://daraz-backend-lsuk.onrender.com", credentials: true }));
 
 app.use(express.json());
 app.use("/sale", saleRouter);
